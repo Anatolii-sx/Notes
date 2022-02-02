952bfa07719c5be34aa2b10078c84b4acc9dafc1
Anatolii-sx <s8.8@bk.ru>
2022-02-02 20:47:41 +0300

    #1 Initial commit. Deleted initial storyboard. Prepared files to the following programming.

---
+++ Notes/App/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Notes
+//
+//  Created by Анатолий Миронов on 02.02.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
